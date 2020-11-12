cf6e4b63f96dfe5fdd7ece0d79bb6978cdd91a01
rares <roman_rares93@yahoo.com>
2020-11-12 21:08:32 +0200

    use hideAllPages function

--- functions.js
+++ functions.js
@@ -1,5 +1,3 @@
-const { addListener } = require("process");
-
 function hide(id) {
     var el = document.getElementById(id);
     console.info("hide:" + id, el);
@@ -11,32 +9,31 @@ function hide(id) {
     }
 }
 
-function showHome() {
+function hideAllPages() {
+    hide("home");
     hide("skills");
     hide("projects");
     hide("languages");
+}
+
+function showHome() {
+    hideAllPages();
     document.getElementById("home").style.display = "";
 }
 
 function showSkills() {
-    hide("projects");
-    hide("languages");
-    hide("home");
+    hideAllPages();
     document.getElementById('skills').style.display = '';
     // document.getElementById('skils-id').style.backgroundColor= '#fff';
     // document.getElementById('skils-id').style.color= 'red';
 }
 
 function showProjects() {
-    hide("skills");
-    hide("languages");
-    hide("home");
+    hideAllPages();
     document.getElementById("projects").style.display = "";
 }
 
 function showLanguages() {
-    hide("projects");
-    hide("skills");
-    hide("home");
+    hideAllPages();
     document.getElementById("languages").style.display = "";
 }
